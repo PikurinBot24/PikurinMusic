66f8c9d9e401f2d6faf1ded2230c1fe72879da59
PikurinBot24 <140874629+PikurinBot24@users.noreply.github.com>
2024-08-23 13:23:07 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -10,7 +10,7 @@ def home():
   return f'<font color="blue", size="7">【{timenow}】PikurinServer用BOT<p>サーバー状況:<p>サーバーは正常です<p><p>もしPikurinBotがオフラインである場合は、サーバーのプログラムでエラーが発生し停止してしまった可能性があります。再起動をお待ちください。'
 
 def run():
-  app.run(host='0.0.0.0', port=1235)
+  app.run(host='0.0.0.0', port=1236)
 
 def keep_alive():
   t = Thread(target=run)
